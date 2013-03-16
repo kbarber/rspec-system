71c7865da638aadb305088acdb1f51eb41975134
Ken Barber <ken@bob.sh>
2013-03-16 02:58:36 -0700

    Output results better
    Signed-off-by: Ken Barber <ken@bob.sh>

--- lib/rspec-system/helpers.rb
+++ lib/rspec-system/helpers.rb
@@ -2,7 +2,7 @@ module RSpecSystem::Helpers
   def run_on(dest, command)
     log.info("run_on(#{dest}, #{command}) executed")
     result = rspec_system_node_set.run(dest, command)
-    log.info("run_on(#{dest}, #{command})\n  result:\n#{result}")
+    log.info("run_on(#{dest}, #{command}) finished\n--result--\n#{result}\n--result--\n")
     result
   end
 end
