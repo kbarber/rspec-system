30937fe2e32ae92bc8e17ab1e2b5913100f3b399
Ken Barber <ken@bob.sh>
2013-03-18 16:27:25 -0500

    Fix rspec-system.rb now that shared_contexts has been removed
    Signed-off-by: Ken Barber <ken@bob.sh>

--- lib/rspec-system.rb
+++ lib/rspec-system.rb
@@ -5,7 +5,6 @@ module RSpecSystem; end
 require 'rspec-system/log'
 require 'rspec-system/helpers'
 require 'rspec-system/node_set'
-require 'rspec-system/shared_contexts'
 
 RSpec::configure do |c|
   c.include RSpecSystem::Helpers
