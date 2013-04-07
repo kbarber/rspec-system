2c483e004687d72865fec576b12d32551328bbe2
Ken Barber <ken@bob.sh>
2013-04-08 00:23:34 +0100

    Fix stdin error from debian
    Signed-off-by: Ken Barber <ken@bob.sh>

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -2,9 +2,16 @@ require 'rspec-system/spec_helper'
 
 RSpec.configure do |c|
   c.include RSpecSystem::Helpers
-  #proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
   # This is where we 'setup' the nodes before running our tests
-  #c.system_setup_block = proc do
-  #end
+  c.system_setup_block = proc do
+    facts = system_node.facts
+
+    # Remove annoying mesg n from profile, otherwise on Debian we get:
+    # stdin: is not a tty which messes with our tests later on.
+    if facts['osfamily'] == 'Debian'
+      log.info("Remove 'mesg n' from profile to stop noise")
+      system_run("sed -i 's/^mesg n/# mesg n/' /root/.profile")
+    end
+  end
 end
