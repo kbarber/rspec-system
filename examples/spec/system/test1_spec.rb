1eb6c76cfe3655b3adabf80eb68420224b712ec4
Ken Barber <ken@bob.sh>
2013-05-04 02:35:03 +0100

    Fix up example
    Signed-off-by: Ken Barber <ken@bob.sh>

--- examples/spec/system/test1_spec.rb
+++ examples/spec/system/test1_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "test1" do
   it 'run test1 - part1' do
-    run_on('main', "cat /etc/resolv.conf")
+    system_run("cat /etc/resolv.conf")
   end
 
   it 'run test1 - part2' do
-    run_on('main', "cat /etc/issue")
+    system_run("cat /etc/issue")
   end
 end
